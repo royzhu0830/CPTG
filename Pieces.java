3bbc712964197ef24feaf1065c21f0ddc70cab0f
Kourosh Jaberi <kouroshjaberi1@gmail.com>
2019-05-21 12:38:09 -0400

    created javadocs of Pieces.java

--- Pieces.java
+++ Pieces.java
@@ -1,35 +1,42 @@
 //testing 123
 public class Pieces {
 	//properties
+	/**name of the piece*/
 	public String strPiece;
+	/**number corresponding to rank of piece; lower rank=stronger*/
 	public int intRank;
+	/**X-coordinate of the piece*/
 	public int intX;
+	/**Y-coordinate of the piece*/
 	public int intY;
 	
 	//methods
+	/**gets the name of the piece*/
 	public String getStrPiece() {
 		return strPiece;
 	}
-	
+	/**gets the rank of the piece*/
 	public int getIntRank() {
 		return intRank;
 	}
+	/**gets the X-coordinate of the piece*/
 	public int getIntX() {
 		return intX;
 	}
+	/**sets the X-coordinate of the Piece*/
 	public void setIntX(int intX) {
 		this.intX=intX;
 	}
+	/**gets the Y-coordinate of the piece*/
 	public int getIntY(){
 		return intY;
 	}
+	/**sets the y-coordinate of the piece*/
 	public void setIntY(int intY) {
 		this.intY=intY;
 	}
-	public void move(){
-		
-	}
-	public void kill(){
+	/**determines outcome of conflict*/
+	public void kill(int intRank){
 		
 	}
 	
